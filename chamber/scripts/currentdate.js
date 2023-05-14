f795c1bcc0a1829e49be891e27cf0e83015241de
ericpvaughn <rozlyntr@gmail.com>
2023-05-13 20:27:27 -0600

    Chamber Push

---
+++ chamber/scripts/currentdate.js
@@ -0,0 +1,10 @@
+
+const datefield = document.querySelector("#currentDate");
+
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
