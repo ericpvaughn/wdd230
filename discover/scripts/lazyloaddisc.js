c8c8e7fef6c9ff8e9d0790531bab9628988cf460
ericpvaughn <rozlyntr@gmail.com>
2023-06-03 12:27:57 -0600

    Discover Page Push

--- discover/scripts/lazyloaddisc.js
+++ discover/scripts/lazyloaddisc.js
@@ -32,20 +32,3 @@ const imgObserver = new IntersectionObserver((items, imgObserver) => {
 images.forEach(image => {
     imgObserver.observe(image);
 })
-
-
-let lastVisit = localStorage.getItem("visit")
-if (!lastVisit) {
-    localStorage.setItem("visit", new Date())
-} else {
-    let currentDate = new Date();
-    let timeStamp1 = currentDate.getTime()
-    let timeStamp2 = new Date(lastVisit).getTime();
-    console.log(timeStamp2)
-    let timeDifference = timeStamp1 - timeStamp2
-    let difference = timeDifference /(1000 * 60 *60 *24)
-    console.log(Math.floor(difference))
-    localStorage.setItem("visit", currentDate)
-    document.querySelector("#visit").textContent = Math.floor(difference)
-}
-
